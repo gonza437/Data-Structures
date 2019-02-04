0bfe0ed6444770b01a056c5c23cd390ec7e5b232
gonza437 <47013187+gonza437@users.noreply.github.com>
2019-02-04 12:21:44 -0800

    Create stack.h
    stack header file

---
+++ stack/stack.h
@@ -0,0 +1,64 @@
+// =======================================================
+// HW#: HW1P1 stack
+// Your name: Omar Gonzalez Martinez
+// Compiler:  g++ 
+// File type: headher file stack.h
+//=======================================================
+
+//----- Globally setting up the aliases ----------------
+
+const int MAX = 10;   // The MAX number of elements for the stack
+                      // MAX is unknown to the client
+
+typedef int el_t;      // the el_t type is ** for now
+                      // el_t is unknown to the client
+//-------------------------------------------------------
+
+
+class stack
+{ 
+  
+ private: // to be hidden from the client
+  
+  el_t     el[MAX];       // el is  an array of el_t's
+  int      top;           // top is index to the top of stack
+  
+ public: // prototypes to be used by the client
+  // Note that no parameter variables are given
+  
+  // exception handling classes  
+  class Overflow{};   // thrown when the stack overflows
+  class Underflow{};  // thrown when the stack underflows
+  
+  stack();   // constructor to create an object
+  ~stack();  // destructor  to destroy an object
+  
+  // PURPOSE: if not full, enters an element at the top;
+  //          otherwise throws an exception - Overflow
+  // PARAMETER: pass the element to be pushed
+  void push(el_t);
+  
+  // PURPOSE: if not empty, removes and gives back the top element;
+  //          otherwise throws an exception - Underflow
+  // PARAMETER: provide variable to receive the popped element (pass by ref)
+  void pop(el_t&);
+  
+  // PURPOSE: if not empty, gives the top element without removing it;
+  //          otherwise, throws an exception - Underflow
+  // PARAMETER: provide variable to return the top element
+  void topElem(el_t&);
+  
+  // ** Must add good comments for each function - See Notes1B
+
+  //PURPOSE: Checks to see if the array is empty; if empty return true else return false
+  bool isEmpty();
+
+  //PURPOSE: Checks to see if the the array is full, if full return true else return false
+  bool isFull();
+
+  //PURPOSE: To display all the elements in the stack
+  void displayAll();
+
+  //PURPOSE: To remove all the elements in the stack
+  void clearIt();
+};
